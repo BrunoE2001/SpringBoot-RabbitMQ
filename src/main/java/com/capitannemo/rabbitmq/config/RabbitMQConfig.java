82cd8338593b4b948fb857b87e3207fed9a02c24
BrunoE2001 <capinemo940@gmail.com>
2024-09-05 13:34:16 -0600

    Doc: Documentacion config package

--- src/main/java/com/capitannemo/rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/capitannemo/rabbitmq/config/RabbitMQConfig.java
@@ -8,6 +8,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/***
+ * RabbitMQConfig
+ * This class is responsible for configuring RabbitMQ components such as queues, exchanges, and bindings.
+ * It uses Spring's @Configuration annotation to enable bean creation and dependency injection.
+ * The configuration is based on the provided properties for queue name, exchange name, and routing key.
+ *
+ * @author CapitanNemo
+ * @since 1.0.0
+ */
 @Configuration
 public class RabbitMQConfig {
 
@@ -20,19 +29,28 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.routing.key}")
     private String routing_key;
 
-    // Spring bean for rabbitmq queue
+    /**
+     * Creates a RabbitMQ queue bean with the name specified in the 'rabbitmq.queue.name' property.
+     * @return a RabbitMQ queue bean
+     */
     @Bean
     public Queue queue() {
         return new Queue(queue);
     }
 
-    // Spring bean for rabbitmq exchange
+    /**
+     * Creates a RabbitMQ topic exchange bean with the name specified in the 'rabbitmq.exchange.name' property.
+     * @return a RabbitMQ topic exchange bean
+     */
     @Bean
     public TopicExchange exchange() {
         return new TopicExchange(exchange);
     }
 
-    // Binding between queue and exchange using routing key
+    /**
+     * Binds the queue to the exchange using the routing key specified in the 'rabbitmq.routing.key' property.
+     * @return a RabbitMQ binding bean
+     */
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(queue())
